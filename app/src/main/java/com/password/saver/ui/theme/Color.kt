e8f27be8cdd4712d5c02f3f1ba63c839b9b30be5
arix2000 <arix20003@gmail.com>
2021-07-07 22:13:52 +0200

    initial setup project

--- app/src/main/java/com/password/saver/ui/theme/Color.kt
+++ app/src/main/java/com/password/saver/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.password.saver.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val ColorPrimary = Color(0xFF834CFF)
+val ColorPrimaryDark = Color(0xFF6200EE)
 val Teal200 = Color(0xFF03DAC5)
