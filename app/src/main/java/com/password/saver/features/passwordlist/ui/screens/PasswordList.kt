1896c200a7e6e5b4021f1c43ea5be14cd1c24f0f
arix2000 <arix20003@gmail.com>
2021-07-11 21:03:58 +0200

    add floating add button

--- app/src/main/java/com/password/saver/features/passwordlist/ui/screens/PasswordList.kt
+++ app/src/main/java/com/password/saver/features/passwordlist/ui/screens/PasswordList.kt
@@ -3,36 +3,44 @@ package com.password.saver.features.passwordlist.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.password.saver.MyPasswordsActivity.Companion.ROUTE_PASSWORD_DETAILS
 import com.password.saver.features.passwordlist.ui.ListItemPassword
+import com.password.saver.features.passwordlist.ui.composables.AddFloatingButton
 import com.password.saver.models.Password
 
 @Composable
 fun PasswordList(passwords: List<Password>, navController: NavController) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-            modifier = Modifier.padding(8.dp)
-        ) {
-            items(passwords) { password ->
-                ListItemPassword(
-                    password = Password(
-                        "Poczta Gmail",
-                        "arix20003@gmail.com",
-                        "Painkiler1"
-                    )
-                ) {
-                    openDetails(navController, password)
+    Box {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.padding(8.dp)
+            ) {
+                items(passwords) { password ->
+                    ListItemPassword(
+                        password = Password(
+                            "Poczta Gmail",
+                            "arix20003@gmail.com",
+                            "Painkiler1"
+                        )
+                    ) {
+                        openDetails(navController, password)
+                    }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(100.dp))
                 }
             }
-            item {
-                Spacer(modifier = Modifier.height(100.dp))
-            }
+        }
+        AddFloatingButton(modifier = Modifier.align(Alignment.BottomEnd)) {
+
         }
     }
 }
